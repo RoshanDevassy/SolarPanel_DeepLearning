728d439f2f3a1615ea0bddab9ca7a6769a5b23ff
Roshan-Devassy <roshandevassy101112@gmail.com>
2025-05-23 18:41:32 +0530

    backend updated

--- backend/api/main.py
+++ backend/api/main.py
@@ -31,7 +31,7 @@ old_FAULT_CLASS_NAMES = ["clean", "dusty", "bird droppings", "snow", "physical d
 app = FastAPI()
 
 origins = [
-    "https://your-frontend.vercel.app",  # ✅ Your deployed frontend URL
+    "https://solarpanel-deeplearning-1.onrender.com",  # ✅ Your deployed frontend URL
 ]
 
 # ✅ Enable CORS for Next.js frontend
